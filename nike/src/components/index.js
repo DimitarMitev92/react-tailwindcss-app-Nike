fa76b0129b9658b4508ae4d884965c3e05a35b7f
Dimitar Mitev <d.mitev920123@gmail.com>
2023-08-18 20:03:48 +0300

    customer reviews section

--- nike/src/components/index.js
+++ nike/src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button.jsx';
 import ShoeCard from './ShoeCard.jsx';
 import PopularProductCard from './PopularProductCard.jsx';
 import ServiceCard from './ServiceCard.jsx';
+import ReviewCard from './ReviewCard.jsx';
 
-export { Button, ShoeCard, PopularProductCard, ServiceCard };
+export { Button, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
