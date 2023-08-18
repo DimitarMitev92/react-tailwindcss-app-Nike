b76373606326b64156020afe2faebb4963c8e2cb
Dimitar Mitev <d.mitev920123@gmail.com>
2023-08-18 19:41:51 +0300

    services section

--- nike/src/components/index.js
+++ nike/src/components/index.js
@@ -1,5 +1,6 @@
 import Button from './Button.jsx';
 import ShoeCard from './ShoeCard.jsx';
 import PopularProductCard from './PopularProductCard.jsx';
+import ServiceCard from './ServiceCard.jsx';
 
-export { Button, ShoeCard, PopularProductCard };
+export { Button, ShoeCard, PopularProductCard, ServiceCard };
